015138657cac3849ff1a178c1e07df2e7c48815a
Jefek <jeffrey.knowles@gmail.com>
2018-10-24 21:26:54 -0400

    Worked on views branch'

--- apps/our_stories/urls.py
+++ apps/our_stories/urls.py
@@ -4,10 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('login.html', views.login),
+    path('home', views.index),
+    path('login', views.login),
     path('process_login', views.process_login),
     path('profile/<int:id>', views.profile),
     path('write', views.write),
     path('write_story/<int:id>', views.write_story),
-    path('explore', views.explore)
+    path('explore', views.explore),
+    path('create', views.create)
     ]
--- apps/our_stories/views.py
+++ apps/our_stories/views.py
@@ -3,8 +3,26 @@ from .models import *
 from django.contrib import messages
 import bcrypt
 
+EMAILREGEX = re.compile(r'^[a-zA-Z0-9.+-]+@[a-zA-Z0-9_.+-]+.[a-zA-Z]+$')
+
+# INDEX PAGE
 def index(request):
-    render(request, 'mainApp/index.html')
+    return render(request, 'our_stories/index.html')
+
+# CREATE NEW USER
+def create(request):
+    errors = User.objects.basic_validator(request.POST)
+    if len(errors):
+        for key, value in errors.items():
+            messages.error(request, value)
+        print(errors)
+        return redirect('/')
+    else:
+        pass_hash = bcrypt.hashpw(request.POST['password'].encode('utf-8'), bcrypt.gensalt())
+        user = User.objects.create(username = request.POST['username'], name = request.POST['name'], password = pass_hash.decode('utf-8'))
+        request.session['id'] = user.id
+        request.session['name'] = user.name
+        return redirect('/dash')
 
 def profile(request, id):
     return HttpResponse("Page under conflagration")
@@ -13,10 +31,30 @@ def write_story(request, id):
     return HttpResponse("Page under conflagration")
 
 def login(request):
-    return HttpResponse("Page under conflagration")
+    return render(request, 'our_stories/login.html')
 
+# LOGOUT PROCESS
+def logout(request):
+    request.session.clear()
+    return redirect('/')
+
+# LOGIN PROCESS
 def process_login(request):
-    return HttpResponse("Page under conflagration")
+    errors = User.objects.login_validator(request.POST)
+    if len(errors):
+        for key, value in errors.items():
+            messages.error(request, value)
+        print(errors)
+        return redirect('/login')
+    else:
+        user = User.objects.get(username=request.POST['username'])
+        if bcrypt.checkpw(request.POST['password'].encode('utf-8'), user.password.encode('utf-8')):
+            request.session['id'] = user.id
+            request.session['username'] = user.username
+            return redirect('/profile')
+        else:
+            messages.error(request, 'Incorrect password.')
+            return redirect('/login')
 
 def write(request):
     return HttpResponse("Page under conflagration")
